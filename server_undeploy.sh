2a4c2c02add4daf1371d4d4a3af12fd2bf309ef0
rashideveloper <bubbless7456@gmail.com>
2022-12-23 07:56:08 +0500

    undeploy from server

--- server_undeploy.sh
+++ server_undeploy.sh
@@ -1,9 +1,7 @@
 project_name=${1:-nothing_qwertyusgh}
 
 sudo rm -rf /var/www/$project_name
-
 sudo rm /etc/nginx/sites-enabled/$project_name
 sudo rm /etc/nginx/sites-available/$project_name
-
 sudo rm /etc/systemd/system/${project_name}.service
 sudo service nginx restart
